d93860bfec3b4eff2fdbc2cc8339dcc77f516f31
Michael Beamish <michaelbeamish89@gmail.com>
2018-09-13 14:51:34 -0700

    Exercise complete.

--- part1/server.js
+++ part1/server.js
@@ -6,6 +6,31 @@ app.get('/yourroute', function(req, res) {
   res.send("stuff");
 });
 
+app.get('/hello', function(req, res) {
+  res.send("Hello!");
+})
+
+app.post('/create/:name', function(req, res) {
+  let obj = {
+    "id":1,
+    "name": req.params.name
+  };
+  res.send(obj);
+})
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+app.get('/verify/:age', function(req, res) {
+  let age = req.params.age;
+  if(age > 13){
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(403);
+  }
+})
+
 app.use(function(req, res) {
   res.sendStatus(404);
 });
--- part2/server.js
+++ part2/server.js
@@ -1,10 +1,44 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+var fs = require('fs');
+
+let userid = 0;
 
 app.get('/yourroute', function(req, res) {
   res.send("stuff");
 });
+
+app.post('/create/:name/:age', function(req, res) {
+  let storage = fs.readFileSync(__dirname + '/storage.json', "utf8");
+  let data = JSON.parse(storage);
+  userid = String(data.length + 1);
+  let obj = {
+    id: userid,
+    name: req.params.name,
+    age: req.params.age
+  };
+  data.push(obj);
+  fs.writeFileSync(__dirname + '/storage.json', JSON.stringify(data));
+  res.send(data);
+});
+
+app.get('/', function(req, res) {
+  let storage = fs.readFileSync(__dirname + '/storage.json', "utf8");
+  let data = JSON.parse(storage);
+  res.send(data);
+})
+
+app.get('/:id', function(req, res) {
+  let storage = fs.readFileSync(__dirname + '/storage.json', "utf8");
+  let data = JSON.parse(storage);
+  data.forEach(element => {
+    if(element["id"] === req.params.id){
+      res.send(element);
+    }
+  });
+  res.sendStatus(400);
+})
 
 app.use(function(req, res) {
   res.sendStatus(404);
